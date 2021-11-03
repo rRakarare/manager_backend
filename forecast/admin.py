412888b74ceaa36cbc01cfaf3ebda223022a67c8
rRakarare <rrene333@googlemail.com>
2021-11-03 14:57:10 +0100

    assaddasdsa

--- forecast/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Charges, Balance
-
-admin.site.register(Charges)
-admin.site.register(Balance)
-
-
--- forecast/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ChargeView, BalanceView
-
-urlpatterns = [  
-    path('charges', ChargeView.as_view(), name='charges'),
-    path('balance', BalanceView.as_view(), name='balance'),
-]
